a6d08e4bca1c426b024530de4cce9643c0f6836d
Dev7896 <barmandev402@gmail.com>
2025-01-02 23:57:26 +0530

    added fadding animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,44 @@
 import author from "../assets/rahul_acharya.jpg";
 import "../styles/Home.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const roles = [
+    "Youtuber",
+    "Fullstack Developer",
+    "Backend Developer",
+    "Singer",
+    "Writer",
+  ]; // Array of roles
+  const [currentRole, setCurrentRole] = useState(0);
+
+  useEffect(() => {
+    // Rotate through the roles every 2 seconds
+    const interval = setInterval(() => {
+      setCurrentRole((prevRole) => (prevRole + 1) % roles.length);
+    }, 2000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  }, [roles.length]);
+
   return (
     <div className="section1">
       <img className="first-image" src={author} alt="author image" />
       <article className="wrapper">
-        <p className="regular-text">Hello I' m</p>
+        <p className="regular-text">Hello I'm</p>
         <h1 className="heading">rahul acharya</h1>
-        <p className="change regular-text">frontend developer</p>
+        {/* Rotating text */}
+        <p className="change regular-text">
+          <span className="animated-text">{roles[currentRole]}</span>
+        </p>
         <div className="btn-group">
           <button>download cv</button>
           <button>contact info</button>
         </div>
         <div className="social-icons">
-          <i class="ri-linkedin-box-fill ri-2x"></i>
-          <i class="ri-github-fill ri-2x"></i>
+          <i className="ri-linkedin-box-fill ri-2x"></i>
+          <i className="ri-github-fill ri-2x"></i>
         </div>
       </article>
     </div>
